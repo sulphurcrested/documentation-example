a727bb61012bfe7ef8335262c41d8000913fecb3
SulphurCrested <144216809+sulphurcrested@users.noreply.github.com>
2023-12-29 20:55:06 +1100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -15,11 +15,14 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'autodoc2', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+autodoc2_render_plugin = "myst"
+autodoc2_packages = [
+    '../src'
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
