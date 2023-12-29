4293c797c7652589e5e6d78fc182f3a59aad13d8
SulphurCrested <144216809+sulphurcrested@users.noreply.github.com>
2023-12-30 01:35:04 +1100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -35,8 +35,32 @@ autodoc2_packages = [
 ]
 autodoc2_output_dir = 'apidoc'
 
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- myst config -------------------------------------------------
+# https://myst-parser.readthedocs.io/en/latest/configuration.html#sphinx-config-options
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "attrs_block",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+myst_heading_anchors = 6
+
